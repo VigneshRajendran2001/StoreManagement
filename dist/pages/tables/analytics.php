ce3093f7735d65c6d170261dabe038c79eee2f6b
VigneshRajendran2001 <pridevignesh@gmail.com>
2024-07-04 15:39:09 +0530

    changes made

--- dist/pages/tables/analytics.php
+++ dist/pages/tables/analytics.php
@@ -373,7 +373,7 @@
     <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">Line chart</h4>
+                    <h4 class="card-title" id="line-charts">Line chart</h4>
                     <canvas id="lineChart"></canvas>
                   </div>
                 </div>
