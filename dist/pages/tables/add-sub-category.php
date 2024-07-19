bbe4123bfb7e02c62919fa2f18ce96bde3eb8689
VigneshRajendran2001 <pridevignesh@gmail.com>
2024-07-19 13:09:55 +0530

    in subcategory , category dropdown changes made

--- dist/pages/tables/add-sub-category.php
+++ dist/pages/tables/add-sub-category.php
@@ -447,37 +447,37 @@
 <script>
   $(document).ready(function () {
 
-        // Fetch categories and populate dropdown
-    //     function fetchCategoriesData() {
-    //       var apikey = 'xgGEHQTWl89KsFPHojMIw7Q3YbACaJwF';
-    //       var pkey = '3fdee6c11c06f9a43fe21eefcdfb5bd7';
-    //     var apiurl = 'https://dev-aniwatch.gateway.apiplatform.io/v1/category'; // Replace with your API endpoint
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: apiurl,
-    //         dataType: 'json',
-    //         contentType: 'application/json',
-    //         headers: {
-    //           'apikey': apikey,
-    //           'pkey': pkey,
-    //         },
-    //         success: function(data) {
-    //             // Clear existing options
-    //             $('#add-subcategory-category').empty();
-    //             // Add default option
-    //             $('#add-subcategory-category').append('<option value="">Select a category</option>');
-    //             // Add each category as an option
-    //             data.forEach(function(category) {
-    //                 $('#add-subcategory-category').append('<option value="' + category.id + '">' + category.category_name + '</option>');
-    //             });
-    //         },
-    //         error: function(xhr, status, error) {
-    //             console.error('Error fetching categories:', error);
-    //         }
-    //     });
-    // }
+        Fetch categories and populate dropdown
+        function fetchCategoriesData() {
+          var apikey = 'xgGEHQTWl89KsFPHojMIw7Q3YbACaJwF';
+          var pkey = '3fdee6c11c06f9a43fe21eefcdfb5bd7';
+        var apiurl = 'https://dev-aniwatch.gateway.apiplatform.io/v1/category'; 
+        $.ajax({
+            type: 'GET',
+            url: apiurl,
+            dataType: 'json',
+            contentType: 'application/json',
+            headers: {
+              'apikey': apikey,
+              'pkey': pkey,
+            },
+            success: function(data) {
+               
+                $('#add-subcategory-category').empty();
+              
+                $('#add-subcategory-category').append('<option value="">Select a category</option>');
+                
+                data.forEach(function(category) {
+                    $('#add-subcategory-category').append('<option value="' + category.id + '">' + category.category_name + '</option>');
+                });
+            },
+            error: function(xhr, status, error) {
+                console.error('Error fetching categories:', error);
+            }
+        });
+    }
 
-    // Call function to fetch categories and populate dropdown on page load
+   
     fetchCategoriesData();
 
     
